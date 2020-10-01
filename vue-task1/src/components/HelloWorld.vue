d548ce571ce76fdd12bc2c4104aaa26a007d2835
satoshi2372 <ape2372@gmail.comgit config --global user.email ape2372@gmail.com>
2020-10-01 11:04:22 +0900

    vue.js　課題１修正

---
+++ vue-task1/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="hello">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "「Hellow World！！」",
+    };
+  },
+};
+</script>
